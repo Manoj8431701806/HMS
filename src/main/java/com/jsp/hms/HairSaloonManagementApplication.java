8421e0e60377788857b6533766c54440e5400a55
DS <DS@DESKTOP-A530GEE>
2025-07-06 12:30:13 +0530

    first commit

---
+++ src/main/java/com/jsp/hms/HairSaloonManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairSaloonManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairSaloonManagementApplication.class, args);
+	}
+
+}
